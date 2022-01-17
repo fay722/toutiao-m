8314c01bedf6db1e1be06041a59b3ca06ed6fd13
jiaoya <jiaoya_926@163.com>
2022-01-17 14:45:30 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import './styles/index.less'
+// 引入vant
+// 核心组件库
+import Vant from 'vant'
+// vant全局样式
+import 'vant/lib/index.css'
+// 注册使用Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
